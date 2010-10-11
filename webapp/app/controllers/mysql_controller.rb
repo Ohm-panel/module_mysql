ca78cfde8b0413b514f42c51eb0ba19cd1f8d316
Joel Cogen <joel@joelcogen.com>
2010-10-11 15:04:16 +0200

    Updated headers

--- webapp/app/controllers/mysql_controller.rb
+++ webapp/app/controllers/mysql_controller.rb
@@ -1,22 +1,21 @@
-# Ohm - Open Hosting Manager <http://ohmanager.sourceforge.net>
-# MySQL module - Main controller
+### Ohm MySQL module ###
 #
-# Copyright (C) 2009-2010 UMONS <http://www.umons.ac.be>
+# Main controller
 #
-# This file is part of Ohm.
+# Copyright (C) 2010 Joel Cogen <http://joelcogen.com>
 #
-# Ohm is free software: you can redistribute it and/or modify
+# This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Ohm is distributed in the hope that it will be useful,
+# This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Ohm. If not, see <http://www.gnu.org/licenses/>.
+# along with this module. If not, see <http://www.gnu.org/licenses/>.
 
 class MysqlController < ApplicationController
   before_filter :authenticate
